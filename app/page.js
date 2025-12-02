8b55d0a209742199b927aec1a9b9579dc776a7e4
Fabio Pereira dos Santos <pereira.fabio.santos@outlook.com>
2025-12-01 23:56:18 -0300

    chore: creating component AboutMe

--- app/page.js
+++ app/page.js
@@ -1,24 +1,4 @@
-const AboutMe = () => {
-  return (
-    <div>
-      <p>
-        Fabio Pereira dos Santos é um desenvolvedor focado principalmente na plataforma .NET
-      </p>
-
-      <p><b>Formação</b></p>
-      <p>Análise e Desenvolvimento de Sistemas, 2014</p>
-      <p>Universidade Anhanguera</p>
-
-      <p><b>Interesses de estudo</b></p>
-      <ul>
-        <li>.NET</li>
-        <li>C# Programing Language</li>
-        <li>Assembly</li>
-        <li>React Native</li>
-      </ul>
-    </div>
-  )
-}
+import AboutMe from './components/AboutMe'
 
 const MyLayout = () => {
   return (
